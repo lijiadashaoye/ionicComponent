5885e5e7bfc3f5d752a6778f1f481d6116d8f6f3
lipeng <lipeng@bicai365.com>
2019-01-21 14:57:12 +0800

    f

--- src/app/isInMainRoute/refresh/refresh.component.ts
+++ src/app/isInMainRoute/refresh/refresh.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./refresh.component.scss']
 })
 export class RefreshComponent {
-  datas = '还未更新'
+  datas = '还未更新';
+
+  customAlertOptions: any = {
+    header: 'Pizza Toppings',
+    subHeader: 'Select your toppings',
+    message: '$1.00 per topping',
+    translucent: true
+  };
+
+  customPopoverOptions: any = {
+    header: 'Hair Color',
+    subHeader: 'Select your hair color',
+    message: 'Only select your dominant hair color'
+  };
+
+  customActionSheetOptions: any = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color'
+  };
+
+  selectChanges(mul) {
+    // multiple:["bacon", "olives", "xcheese"]
+
+    console.log(mul.value)    
+  }
+
+  // 下拉刷新
   doRefresh(event) {
     setTimeout(() => {
       this.datas = '已经更新'
